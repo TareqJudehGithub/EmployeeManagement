f967f9e4189d5dabc9ccf0ab9a98b013938a03ab
TareqJudehGithub <tareq.joudeh@gmail.com>
2023-03-05 21:36:42 +0200

    Added Different route actions methods: string, json, ObjectResult, and ViewResult.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,16 +30,33 @@ namespace EmployeeManagement.Controllers
         }
         // Custom routes/actions
         public string Details()
+        // string action
         {
+
             return "This is Details";
         }
 
+        // JsonResult action
         public JsonResult JsonDetails()
         {
+            // return data of type Employee
             Employee employee = _employeeRespository.GetEmployee(102);
             // also works: var employee = _employeeRespository.GetEmployee(102);
             return Json(employee);
         }
+        public ObjectResult ObjectResultDetails()
+        // Object result action
+        {
+            Employee employee = _employeeRespository.GetEmployee(103);
+            return new ObjectResult(employee);
+        }
+        public ViewResult ViewResultsDetails()
+        {
+            // returns a view
+            Employee employee = _employeeRespository.GetEmployee(101);
+            return View(employee);
+        }
+
         #endregion
 
     }
